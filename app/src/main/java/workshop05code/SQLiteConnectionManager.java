6ecccb50d1e717b7242a07792f9574bec8c00cf8
RepentantDepletion <isaac.bella1@students.mq.edu.au>
2024-03-21 19:11:58 +1100

    Prac changes

--- app/src/main/java/workshop05code/SQLiteConnectionManager.java
+++ app/src/main/java/workshop05code/SQLiteConnectionManager.java
@@ -145,10 +145,12 @@ public class SQLiteConnectionManager {
      * @return true if guess exists in the database, false otherwise
      */
     public boolean isValidWord(String guess) {
+        String category = "1";
         String sql = "SELECT count(id) as total FROM validWords WHERE word like'" + guess + "';";
 
         try (Connection conn = DriverManager.getConnection(databaseURL);
                 PreparedStatement stmt = conn.prepareStatement(sql)) {
+                    stmt.setString(1, category);
 
             ResultSet resultRows = stmt.executeQuery();
             if (resultRows.next()) {
